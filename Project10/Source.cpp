80a275789d4b82943580692f6a7e2435deb7442e
zozrom <zozulia.roman135@gmail.com>
2018-12-24 14:39:15 +0200

    second commit

--- Project10/Source.cpp
+++ Project10/Source.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
+#include <cstdlib>
 using namespace std;
+void play(int x);
 int main()
 {
-	cout << " Let's start ! ";
-	cout << "Game guess the magic number";
+	int opinion;
+	int magic_numbers;
+	magic_numbers = rand() % 20;
+	do
+	{
+		cout << "1:  Get the magic numbers. " << "\t" << "\t" << "\t" << "\t";
+		cout << "2: I want play! " << "\t" << "\t" << "\t";
+		cout << "3: Exit with game. " << endl;
+		cout << "\t" << "\t" << "\t";
+		cout << endl;
+		cout << "Enter the number :";
+			cin >> opinion;
+
+	} while (opinion<1 || opinion>3 );
+	
 
 	return 0;
  }
